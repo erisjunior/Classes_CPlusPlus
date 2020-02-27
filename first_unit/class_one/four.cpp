7be7795d3f603c0a680246fbdefdae9ee38187c3
ANTONIO ERISVAN ALVES JUNIOR <eris.junior@A305-39L.imd.local>
2020-02-27 15:35:02 -0300

    fix: calc variancy correctly

--- first_unit/class_one/four.cpp
+++ first_unit/class_one/four.cpp
@@ -8,29 +8,31 @@ using std::endl;
 
 int main(int argc, char* argv[]) {
 	int total, sum = 0;
+  float variancy;
 
 	cin >> total;
 
 	int * numbersPtr = new int[total];
-	
+
 	for (int i = 0; i < total; i++) {
 		cin >> numbersPtr[i];
 		sum += numbersPtr[i];
 	}
 
-	calcVariancy(numbersPtr, total, sum);
+	variancy = calcVariancy(numbersPtr, total, sum);
+
+	cout << variancy << endl;
 
 	return 0;
 }
 
 float calcVariancy(int * numbersPtr, int total, int sum) {
-	int variancy;
-	
-	for (int i = 0; i < total; i++) {
-		variancy += (numbersPtr[i] - sum) * (numbersPtr[i] - sum);
-	}
+	float variancy = 0;
+  int mean = sum/total;
 
-	cout << variancy/total << endl;
+	for (int i = 0; i < total; i++) {
+		variancy += (numbersPtr[i] - mean) * (numbersPtr[i] - mean);
+	}
 
 	return variancy/total;
 }
